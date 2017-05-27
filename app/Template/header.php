d1052612c1e1dad5c718d33fc1769f47243ea3f6
Wisnu Hafid <inuvalogic@gmail.com>
2017-05-27 23:12:53 +0700

    assets helper

--- app/Template/header.php
+++ app/Template/header.php
@@ -8,10 +8,12 @@
 
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 
+<?= $akane['asset']->css('theme.css') ?>
+
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
-<body>
+<body class="bg-gray">
     <header>
         <h1>Akane Framework<sup>v3</sup></h1>
     </header>
