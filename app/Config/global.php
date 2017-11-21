30d963c527398418afe0ae840d2af986c2e99a80
Wisnu Hafid <inuvalogic@gmail.com>
2017-11-21 19:47:27 +0700

    fixing global define config

--- app/Config/global.php
+++ app/Config/global.php
@@ -1,7 +1,17 @@
 <?php
 
-define('__SITEURL__', 'http://'.$_SERVER['HTTP_HOST']);
+if (!defined('__SITEURL__')){
+	define('__SITEURL__', 'http://'.$_SERVER['HTTP_HOST']);
+}
 
-define('DS', DIRECTORY_SEPARATOR);
-define('CONFIG_DIR' , __DIR__ . '..' . DS . '..' . DS . 'Config' . DS);
-define('APP_CONFIG_DIR' , 'app' . DS . 'Config' . DS);
+if (!defined('DS')){
+	define('DS', DIRECTORY_SEPARATOR);
+}
+
+if (!defined('CONFIG_DIR')){
+	define('CONFIG_DIR' , __DIR__ . '..' . DS . '..' . DS . 'Config' . DS);
+}
+
+if (!defined('APP_CONFIG_DIR')){
+	define('APP_CONFIG_DIR' , 'app' . DS . 'Config' . DS);
+}
