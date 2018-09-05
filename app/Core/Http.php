8d211dc226010447addb5e8ddeb107dba6b0ce38
Wisnu Hafid <wisnuhafid@gmail.com>
2018-09-05 10:29:55 +0700

    helper get ip address

--- app/Core/Http.php
+++ app/Core/Http.php
@@ -27,4 +27,17 @@ class Http
             }
         }
     }
+
+    public static function getIPAddress()
+    {
+        if (isset($_SERVER['HTTP_CLIENT_IP']) && !empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && !empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'];
+        }
+
+        return $ip;
+    }
 }
